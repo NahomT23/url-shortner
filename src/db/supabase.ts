256b317db62c0f9505df42cf131db2dc1378edd7
Nahom Tsegaye <tnahom214@gmail.com>
2025-02-19 19:59:45 +0300

    gitignore updated

--- src/db/supabase.ts
+++ src/db/supabase.ts
@@ -5,7 +5,6 @@ const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-console.log(supabaseKey)
 
 export default supabase
         
